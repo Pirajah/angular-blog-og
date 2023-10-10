ecc6565524f82bc3d88746aa803968013e7c8c0b
Pirajah <pirajah@outlook.com>
2023-10-10 13:13:24 -0300

    Clona mas não faz igual

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,8 +1,8 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "title": "Aluno atrasado clona repositório de professor!",
+    "description": "Estudante alega : Essa porcaria de angular tava na versão errada!",
     "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
   {
